40272ee01342adc8f65dc4358f48d1f25894cc7f
Celine Prakash <celi0001@hotmail.com>
2024-08-23 13:21:04 +0200

    updated run script and README for CeMM cluster usage (#6)

--- README.md
+++ README.md
@@ -25,6 +25,37 @@ Patricia provided us with a sample sbatch submission script (file `rstudio-singu
 >
 >Patricia
 
+## Rstudio server on CeMM's cluster
+
+Adapted Patricia's script to isolate different RStudio sessions based on the working directory of the session. The following changes were made:
+
+* Slurm standard out log file gets written out to the directory where the script is run
+* Working directory is set as the path from where the script is run
+* The path to the singularity image is specified to the variable `singularityimage` at the top of the script, which is used in the singularity exec command.
+* In the `SINGULARITY_BIND` variable, the path of the working directory on the CeMM cluster is bound to /home/$(whoami) in RStudio, instead of binding /home of the CeMM cluster to /home in RStudio
+
+### Usage
+
+1. Update the variables in the script to match your image name
+2. Run `sbatch rstudio-singularity-password.bash`
+3. Look into the file rstudio-singularity-%j.log which will have the following line right at the top 
+   "Running RStudio at `machineIPaddress:port`"
+4. In a terminal, enter the following command
+   `ssh -N -f -L localhost:<port>:localhost:<port> <username>@<machineIPaddress>`
+5. Open localhost:<port> in your browser
+6. Login to the session with your username and the password specified in the run script 
+
+### Rstudio server images available
+
+**Singularity image of our docker image dockrstudio_4.2.0**
+/nobackup/lab_ccri_bicu/public/apptainer_images/ccribioinf_dockrstudio_4.2.0-v1.sif
+(DESeq2 is not able to be installed in the image due to `zlib.h` missing)
+
+**Rocker tidyverse 4.4 image, built by Aleks**
+/nobackup/lab_ccri_bicu/public/apptainer_images/tidyverse-4.4-jdk.sif
+(DESeq2 installs successfully with this)
+
+
 
 ## Rstudio server on CCRI's machines
 
--- rstudio-singularity-password.bash
+++ rstudio-singularity-password.bash
@@ -5,14 +5,15 @@
 #SBATCH --mem=8G
 #SBATCH --time 12:00:00
 #SBATCH --job-name rstudio-singularity
-#SBATCH --output /nobackup/<yourgroup>/<youruser>/rstudio/rstudio-singularity-%j.log
+#SBATCH --output rstudio-singularity-%j.log
 
 module load singularity
 
 # set the work directory to your group
 # set the version to the version that you pulled
-workdir=/nobackup/<yourgroup>/<youruser>/rstudio/rstudio-tmp
-version=4.2
+workdir=$(pwd)
+version=4.4
+singularityimage=/nobackup/lab_ccri_bicu/public/apptainer_images/tidyverse-4.4-jdk.sif
 
 mkdir -p -m 700 ${workdir}/run ${workdir}/tmp ${workdir}/var/lib/rstudio-server ${workdir}/R/$version
 
@@ -26,14 +27,14 @@ END
 # Set R_LIBS_USER to a path specific to rocker/rstudio to avoid conflicts with
 # personal libraries from any R installation in the host environment
 
-export SINGULARITY_BIND="${workdir}/run:/run,${workdir}/tmp:/tmp,${workdir}/rsession.conf:/etc/rstudio/rsession.conf,${workdir}/var/lib/rstudio-server:/var/lib/rstudio-server,${workdir}/run:/var/run,/nobackup:/nobackup,/research:/research,/home:/home"
+export SINGULARITY_BIND="${workdir}/run:/run,${workdir}/tmp:/tmp,${workdir}/rsession.conf:/etc/rstudio/rsession.conf,${workdir}/var/lib/rstudio-server:/var/lib/rstudio-server,${workdir}/run:/var/run,/nobackup:/nobackup,/research:/research,${workdir}:/home/$(whoami)"
 
 # Do not suspend idle sessions.
 # Alternative to setting session-timeout-minutes=0 in /etc/rstudio/rsession.conf
 # https://github.com/rstudio/rstudio/blob/v1.4.1106/src/cpp/server/ServerSessionManager.cpp#L126
 export SINGULARITYENV_RSTUDIO_SESSION_TIMEOUT=0
 export SINGULARITYENV_USER=${USER}
-export SINGULARITYENV_PASSWORD='pleasemakeagoodpassword'
+export SINGULARITYENV_PASSWORD='test1'
 
 # Get unused socket per https://unix.stackexchange.com/a/132524
 # Tiny race condition between the python & singularity commands
@@ -45,7 +46,7 @@ cat 1>&2 <<END
 "Running RStudio at $ADD:$PORT"
 END
 
-singularity exec --cleanenv rstudio-$version.simg \
+singularity exec --cleanenv ${singularityimage} \
     rserver --www-port ${PORT} \
             --server-user=$(whoami) \
             --auth-none=0 \
